30f148194f6e53f7bbe6a2f872367cf4df4b2d82
dwood <dwood15@users.noreply.github.com>
2026-01-05 20:40:14 -0700

    Adding omitempty fields to reduce json bloat

--- events.go
+++ events.go
@@ -491,19 +491,3 @@ type SubscriptionUpdate struct {
 type SubscriptionDelete struct {
 	*Subscription
 }
-
-type MessagePollVoteAdd struct {
-	UserID    string `json:"user_id"`
-	ChannelID string `json:"channel_id"`
-	MessageID string `json:"message_id"`
-	GuildID   string `json:"guild_id,omitempty"`
-	AnswerID  int    `json:"answer_id"`
-}
-
-type MessagePollVoteRemove struct {
-	UserID    string `json:"user_id"`
-	ChannelID string `json:"channel_id"`
-	MessageID string `json:"message_id"`
-	GuildID   string `json:"guild_id,omitempty"`
-	AnswerID  int    `json:"answer_id"`
-}
